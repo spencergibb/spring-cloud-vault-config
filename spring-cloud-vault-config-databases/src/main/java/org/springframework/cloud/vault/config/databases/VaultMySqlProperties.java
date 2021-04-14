86c0d470bd12c17c7ff6170ddd088b0c03e4a30c
Quintin Beukes <quintinb@discovery.co.za>
2021-04-14 13:28:29 +0200

    javadoc correction
    Change-Id: I649b8e48d11022429346cc13d0eae7d45db6ef41

--- spring-cloud-vault-config-databases/src/main/java/org/springframework/cloud/vault/config/databases/VaultMySqlProperties.java
+++ spring-cloud-vault-config-databases/src/main/java/org/springframework/cloud/vault/config/databases/VaultMySqlProperties.java
@@ -25,7 +25,7 @@ import org.springframework.lang.Nullable;
  *
  * @author Mark Paluch
  * @deprecated since 2.0. Use {@link VaultDatabaseProperties} or
- * {@link VaultMultipleDatabaseProperties}.
+ *  {@link VaultMultipleDatabaseProperties}.
  */
 @ConfigurationProperties("spring.cloud.vault.mysql")
 @Deprecated
